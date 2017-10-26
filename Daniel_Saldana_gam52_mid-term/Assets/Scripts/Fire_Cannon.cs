ed9d326bb8e249141c03fcdbf8df70fe32890da3
Daniel <daniel.e.saldana@gmail.com>
2017-10-25 17:16:53 -0700

    Added Cannon w/ Shoot Anim. Added Singleton for score Keeping.
    -Press SPACE to shoot
    -ScoreManager object in scene shows Score in Inspector

---
+++ Daniel_Saldana_gam52_mid-term/Assets/Scripts/Fire_Cannon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire_Cannon : MonoBehaviour 
+{
+	Animator anim;
+
+	void Start()
+	{
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update ()
+	{
+		Shoot ();
+	}
+	public void Shoot()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			anim.SetTrigger("Fire");
+			ScoreManager.instance.score++;
+		}
+	}
+}
---
+++ Daniel_Saldana_gam52_mid-term/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static ScoreManager instance = null;
+
+	public int score;
+
+	void Awake ()
+	{
+		if (instance == null) 
+		{
+			instance = this;
+		}
+	}
+}
